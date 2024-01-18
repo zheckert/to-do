7c92702bd9b12649b4ec6dbdda0a39c75bcf4703
Zach Heckert <zach@zachheckert.com>
2024-01-18 17:14:43 -0500

    Fix server. Adjust related dependencies

--- server.js
+++ server.js
@@ -10,24 +10,18 @@ const port = process.env.PORT || 9001
 
 app.use(express.json())
 app.use(morgan("dev"))
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "client", "public")))
 
-mongoose.connect(process.env.MONGODB_URI,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    },
-    () => console.log("Connected to database")
-)
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to the database"))
+    .catch(error => console.error("Error connecting to the database:", error));
 
 app.use("/todo", require("./routes/todoRouter.js"))
 
 app.use((error, req, res, next) => {
-    console.log(error)
-    return res.send({errorMessage: error.message})
-})
+    console.error(error);
+    res.status(500).json({ errorMessage: error.message });
+ });
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
